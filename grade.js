65bb20d8aff824e7f5f9f598871ce421d0f127f4
Enock Tangus <tanguskipngeno@gmail.com>
2025-03-12 20:24:19 +0300

    feat: update code comments

--- grade.js
+++ grade.js
@@ -1,9 +1,11 @@
-// calculateGrade uses ternary operator to determine the grade based on the value of the .
+// calculateGrade function uses ternary operator to determine the grade based on the value of the students' marks.
 function calculateGrade() {
     let marks = Number(prompt("Enter student marks (between 0 and 100):"));
+// Input validation to ensure that the entered value or input is between 0 and 100.
     if (isNaN(marks) || marks < 0 || marks > 100) {
         return "Invalid input (please enter a number between 0 and 100).";
     }
+// ternary operator to determine the grades.
     return marks > 79
     ? grade = "A"
     : marks >= 60 && marks <= 79
